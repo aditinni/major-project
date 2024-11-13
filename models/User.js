df46d5289a361f08d418efc166fe25c8ba4ebb07
@Aditya.R_11 <121797587+aditinni@users.noreply.github.com>
2024-11-13 21:30:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const domains = [
+  "Web Development",
+  "App Development",
+  "Data Structures",
+  "Computer Networks",
+  "Operating System"
+];
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true }, // Make email unique
-    password: String,
-    level: { type: String, enum: ['beginner' ,'intermediate' ,'advanced'] }, // 1, 2, 3, or 4
-    domainOfInterest: { type: String, enum: ['Web Development', 'App Development', 'Machine Learning', 'JAVA', 'C++', 'DSA'] },
-    languageMode: { type: String, enum: ['English', 'Hindi'] }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  engineeringYear: {
+    type: Number,
+  },
+  domainOfInterest: {
+    type: String,
+    enum: domains,
+  },
+  topicsCompleted: {
+    type: [String],
+    default: [],
+    validate: {
+      validator: function (array) {
+        return array.length === new Set(array).size;
+      },
+      message: "topicsCompleted should contain unique values.",
+    },
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
